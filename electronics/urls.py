f4a3ab0b67d49a1cc78c7aa4e69d8b448f603f16
Shubham <shubhamadak332@gmail.com>
2025-03-20 17:06:38 +0530

    third commit

--- electronics/urls.py
+++ electronics/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from electronics import views
 
 urlpatterns = [
-    path('product/', views.product_detail, name = 'product_detail')
+    path('product/', views.product_detail, name = 'product_detail'),
+    path('update/<int:id>', views.update_product, name = 'update_product'),
+    path('delete_product/<int:id>', views.delete_product, name = 'delete_product'),
 ]
--- electronics/views.py
+++ electronics/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product 
 from .forms import ProductForm
 
@@ -15,4 +15,22 @@ def product_detail(request):
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
+            form = ProductForm()
     return render(request, 'electronics/product_detail.html', {'prod': prod, 'form':form})
+
+def update_product(request, id):
+    prod = get_object_or_404(Product, id=id)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=prod)
+        if form.is_valid():
+            form.save()
+            return redirect('product_detail')
+    else:
+        form = ProductForm(instance=prod)
+    return render(request, 'electronics/update_product.html', {'form': form, 'prod': prod})
+
+def delete_product(request, id):
+    prod = get_object_or_404(Product, id=id)
+    prod.delete()
+    return redirect('product_detail')
